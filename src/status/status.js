c5ebc58cc521ab4491ecc7baee2931f0bd3a12f6
LFCfan25 <66164881+LFCfan25@users.noreply.github.com>
2020-08-27 19:47:28 +0100

    Create status.js

---
+++ src/status/status.js
@@ -0,0 +1,7 @@
+Const config = require(./src/status/config.json)
+Const timer = require(config.timer)
+Const ForReading = 1
+Const ForWriting = 2
+
+module.exports = function({ bot, knex, config, commands }) {setInterval(__,);
+}
