c5712f3d7d373a537528aea5372c3431a9061a8c
Eight8 <66164881+Eight8-7020@users.noreply.github.com>
2020-09-10 21:48:01 +0100

    Update status.js

--- src/status/status.js
+++ src/status/status.js
@@ -1,13 +1,8 @@
-//requiring the statuscfg.json and also the status object
+module.exports = function({ bot, knex, config, commands }) {
 Const statuscfg = require(./src/status/statuscfg.json)
 Const statusmsg = require(statuscfg.status)
-
-// status interval
-function intervalFunc(for (i in myObj.statusmsg) {
+  function intervalFunc(for (i in myObj.statusmsg) {
   x += myObj.statusmsg[i];
-}config.status = statusmsg.;)
-
-//exporting the function
-module.exports = function({ bot, knex, config, commands }) {
-setInterval(intervalFunc, statuscfg.time)
+  }config.status = statusmsg.;);
+  setInterval(intervalFunc, statuscfg.time)
 }
